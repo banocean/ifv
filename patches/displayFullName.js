15b6076b7f7e79224ea34a98581e50641052fc40
PanLiszka <michalek.muzyka@gmail.com>
2024-08-27 11:39:42 +0200

    delete illegal return

--- patches/displayFullName.js
+++ patches/displayFullName.js
@@ -1,15 +1,20 @@
 let studentData = document.querySelector(".side_student");
 if (window.location.hostname.match(/(dziennik-)?wiadomosci.*/))
-  studentData = document.querySelector(".account__name span").firstChild;
+    studentData = document.querySelector(".account__name span").firstChild;
 
-if (!studentData.textContent) return;
+if (studentData.textContent) {
+    const studentNameSpan = document.createElement("span");
+    studentNameSpan.style = "font-size: 20px;";
+    studentNameSpan.innerHTML = `${studentData.textContent}`;
 
-const studentNameSpan = document.createElement("span");
-studentNameSpan.style = "font-size: 20px;";
-studentNameSpan.innerHTML = `${studentData.textContent}`;
+    const usernameContainer = document.querySelector(
+        ".user div:nth-of-type(2)",
+    );
+    usernameContainer.style =
+        "display: flex; flex-direction: column; font-size: 16px;";
 
-const usernameContainer = document.querySelector(".user div:nth-of-type(2)");
-usernameContainer.style =
-  "display: flex; flex-direction: column; font-size: 16px;";
-
-usernameContainer.insertBefore(studentNameSpan, usernameContainer.firstChild);
+    usernameContainer.insertBefore(
+        studentNameSpan,
+        usernameContainer.firstChild,
+    );
+}
