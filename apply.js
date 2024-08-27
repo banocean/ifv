9194fdd605ec9d2c5423d333be9984e848acf31e
banocean <47253870+banocean@users.noreply.github.com>
2024-08-27 09:50:25 +0200

    implement patches system

---
+++ apply.js
@@ -0,0 +1,26 @@
+const jsPatches = []
+const cssPatches = ["hideWCAG"]
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+        for (const patch of jsPatches) {
+            chrome.scripting.executeScript({
+                target: { tabId: tabId },
+                files: [`./patches/${patch}.js`]
+            })
+        }
+    }
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'loading' && /^http/.test(tab.url)) {
+        for (const patch of cssPatches) {
+            chrome.scripting.insertCSS({
+                target: {tabId: tabId},
+                files: [`./patches/${patch}.css`]
+            })
+        }
+    }
+})
+
+// "*://*dziennik-uczen.vulcan.net.pl/*"
