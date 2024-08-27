0ded601a126e53b985848b56207b84c49b12aa65
PanLiszka <michalek.muzyka@gmail.com>
2024-08-27 11:13:11 +0200

    add displayFullName patch

--- apply.js
+++ apply.js
@@ -1,26 +1,26 @@
-const jsPatches = []
+const jsPatches = ["displayFullName"]
 const cssPatches = ["hideWCAG"]
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
-        for (const patch of jsPatches) {
-            chrome.scripting.executeScript({
-                target: { tabId: tabId },
-                files: [`./patches/${patch}.js`]
-            })
-        }
+  if (changeInfo.status === 'complete' && /^http/.test(tab.url)) {
+    for (const patch of jsPatches) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: [`./patches/${patch}.js`]
+      })
     }
+  }
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'loading' && /^http/.test(tab.url)) {
-        for (const patch of cssPatches) {
-            chrome.scripting.insertCSS({
-                target: {tabId: tabId},
-                files: [`./patches/${patch}.css`]
-            })
-        }
+  if (changeInfo.status === 'loading' && /^http/.test(tab.url)) {
+    for (const patch of cssPatches) {
+      chrome.scripting.insertCSS({
+        target: { tabId: tabId },
+        files: [`./patches/${patch}.css`]
+      })
     }
+  }
 })
 
 // "*://*dziennik-uczen.vulcan.net.pl/*"
