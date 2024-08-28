ff5dad3f98033f5e7fe9d4a62a50d7b9754b07d5
PanLiszka <michalek.muzyka@gmail.com>
2024-08-28 15:26:26 +0200

    added cursor: pointer to logo on dziennik-uczen.vulcan.net.pl

--- patches/redirectToBoard/script.js
+++ patches/redirectToBoard/script.js
@@ -1,48 +1,48 @@
-const isEduVulcan = !window.location.hostname.startsWith("dziennik");
-const isMobile = window.innerWidth < 1024;
+const isEduVulcan = !window.location.hostname.startsWith('dziennik')
+const isMobile = window.innerWidth < 1024
 
 const getLogoElement = () =>
-    document.querySelector(".header__logo-product").firstChild;
+    document.querySelector('.header__logo-product').firstChild
 
 const logoObserver = new MutationObserver((mutationsList, observer) => {
     if (getLogoElement()) {
-        observer.disconnect();
-        redirectToBoard();
+        observer.disconnect()
+        redirectToBoard()
     }
-});
+})
 
 function redirectToBoard() {
-    const logoElement = getLogoElement();
+    const logoElement = getLogoElement()
     if (!!window.location.hostname.match(/^(dziennik-)?wiadomosci.*/)) {
         const url = `https://${window.location.hostname.replace(
-            "wiadomosci",
-            "uczen",
-        )}/${window.location.pathname.split("/")[1]}/App`;
+            'wiadomosci',
+            'uczen'
+        )}/${window.location.pathname.split('/')[1]}/App`
 
-        if (isEduVulcan) logoElement.href = url;
+        if (isEduVulcan) logoElement.href = url
         else {
-            logoElement.onclick = () => (window.location.href = url);
-            logoElement.style = "cursor: pointer;";
+            logoElement.onclick = () => (window.location.href = url)
+            logoElement.style = 'cursor: pointer;'
         }
     } else if (isMobile) {
-        if (isEduVulcan) logoElement.href = "javascript:void(0)";
+        if (isEduVulcan) logoElement.href = 'javascript:void(0)'
 
         logoElement.onclick = () => {
-            document.querySelector(".app").classList.add("hideAside");
-            document.querySelector(".header__hamburger__icon button").click();
-            document.querySelector(".tablica a").click();
-            document.querySelector(".app").classList.remove("hideAside");
-        };
+            document.querySelector('.app').classList.add('hideAside')
+            document.querySelector('.header__hamburger__icon button').click()
+            document.querySelector('.tablica a').click()
+            document.querySelector('.app').classList.remove('hideAside')
+        }
     } else {
-        if (isEduVulcan) logoElement.href = "javascript:void(0)";
-        logoElement.onclick = () =>
-            document.querySelector(".tablica a").click();
+        if (isEduVulcan) logoElement.href = 'javascript:void(0)'
+        else logoElement.style = 'cursor: pointer;'
+        logoElement.onclick = () => document.querySelector('.tablica a').click()
     }
 }
 
 if (getLogoElement()) {
-    redirectToBoard();
+    redirectToBoard()
 } else
     logoObserver.observe(document.body, {
         subtree: true,
-    });
+    })
