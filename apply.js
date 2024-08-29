61dc4ce516bc99b03cfd74d191d2bf5e4fb34d9a
Kacper Majcher <majchermajus@proton.me>
2024-08-29 15:43:54 +0200

    Hide help block on dashboard (#10)
    Co-authored-by: BanOcean <47253870+banocean@users.noreply.github.com>

--- apply.js
+++ apply.js
@@ -42,9 +42,16 @@ const patches = [
     description: "todo",
     files: {
       js: ["redirectToBoard/script.js"],
-      css: ["redirectToBoard/styles.css"]
+      css: ["redirectToBoard/styles.css"],
     },
-  }
+  },
+  {
+    name: "Hide Help On Dashboard",
+description: "Hides \"Do you need help\" tile",
+    files: {
+      css: ["hideHelpOnDashboard.css"],
+    },
+  },
 ];
 
 let config = {
