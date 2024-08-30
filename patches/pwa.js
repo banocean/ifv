2ce04de8c86e624df2f63309120731ac39f141b0
PanLiszka <michalek.muzyka@gmail.com>
2024-08-30 10:05:22 +0200

    changed suggested things

--- patches/pwa.js
+++ patches/pwa.js
@@ -4,7 +4,6 @@ function injectWebManifest() {
     metaLink.setAttribute("rel", "manifest");
     metaLink.setAttribute(
         "href",
-        // TODO: implement web manifest for UONET+
         "https://raw.githubusercontent.com/banocean/ifv/main/pwa/manifest-eduvulcan.json",
     );
 
@@ -15,5 +14,5 @@ window.modules.push({
     isLoaded: () => true,
     onlyOnReloads: true,
     run: injectWebManifest,
-    doesRunHere: () => ["eduvulcan.pl", "dziennik-uczen.vulcan.net.pl"].includes(window.location.hostname),
+    doesRunHere: () => ["eduvulcan.pl"].includes(window.location.hostname),
 });
