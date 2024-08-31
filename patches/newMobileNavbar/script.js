ed53e97d1df3553d4cf0194bb191eeb6f459fc98
banocean <47253870+banocean@users.noreply.github.com>
2024-08-31 14:07:36 +0200

    finish more page

--- patches/newMobileNavbar/script.js
+++ patches/newMobileNavbar/script.js
@@ -24,21 +24,35 @@ const getPages = () => {
     })
 }
 
+const BACK_ICON_URL = "https://raw.githubusercontent.com/banocean/ifv/new-navbar/assets/icons/keyboard_backspace_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg"
+
 const run = () => {
     document.querySelector(".header__hamburger__icon button").click()
 
     const nav = document.createElement("nav")
+    nav.innerHTML =
+        `
+        <div>
+            
+        </div>
+        `
     nav.classList.add(".bottom-navigation-bar")
 
     const more = document.createElement("div")
     more.classList.add("more-popup")
-    more.innerHTML = "<div><img src=''</div><h1>Więcej</h1></div><div></div>"
+    more.innerHTML = `<div><img src='${BACK_ICON_URL}'><h1>Więcej</h1></div><div></div>`
 
-    const ignoredPages = []
+    more.querySelector("img").addEventListener("click", () => {
+        more.style.display = "none"
+    })
+
+    const ignoredPages = ["tablica", "oceny", "frekwencja", "planZajec"]
     const pages = getPages()
 
-    console.log(pages)
     for (const page of pages) {
+        const itemClass = Array.from(page.element.classList)
+            .find((c) => !["MuiListItem-root", "MuiListItem-gutters", "selected"].includes(c))
+        if (ignoredPages.includes(itemClass)) continue
         const item = document.createElement("div")
         item.innerHTML = "<div class='icon'></div><span class='name'></span>"
         item.querySelector(".icon").style.content = page.icon
@@ -46,10 +60,12 @@ const run = () => {
         if (page.type === 1) item.addEventListener("click", () => {
             page.element.querySelector("a").click()
             more.style.display = "none"
+            document.querySelector(".header__hamburger__icon button").click()
         })
         more.querySelector("div:last-of-type").appendChild(item)
     }
 
+    document.body.appendChild(nav)
     document.body.appendChild(more)
 }
 
