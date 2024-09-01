0cb27f703010110527c887e58d47a79281059e19
Marioneq 4958 <mq4958@gmail.com>
2024-09-01 18:39:27 +0200

    (fix): config isn't preserved after closing browser (#21)

--- apply.js
+++ apply.js
@@ -104,104 +104,112 @@ const patches = [
   },
 ];
 
-let config = {
-  ...patches.reduce(
-    (acc, patch) => ({
-      ...acc,
-      [patch.name]: { description: patch.description, enable: true },
-    }),
-    {}
-  ),
-};
-
-chrome.storage.sync.set({ options: config });
-
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-  if (namespace !== "sync") return;
-  config = changes.options.newValue;
-  chrome.permissions.getAll((permissions) => {
-    const hostPatterns = permissions.origins || [];
-
-    hostPatterns.forEach((pattern) => {
-      chrome.tabs.query({ url: pattern }, (tabs) => {
-        tabs.forEach((tab) => {
-          chrome.tabs.reload(tab.id);
-        });
-      });
-    });
-  });
-});
-
 const allowedHostnames = [
   "dziennik-uczen.vulcan.net.pl",
   "dziennik-wiadomosci.vulcan.net.pl",
   "uczen.eduvulcan.pl",
   "wiadomosci.eduvulcan.pl",
-  "eduvulcan.pl"
-]
+  "eduvulcan.pl",
+];
 
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-  if (changeInfo.status !== "loading" || !/^http/.test(tab.url)) return
-  const tabHostname = (new URL(tab.url)).hostname
-  if (!allowedHostnames.includes(tabHostname)) return
+async function run() {
+  let config = (await chrome.storage.sync.get("options"))?.options ?? {};
 
-  chrome.scripting.insertCSS({
-    target: { tabId: tabId },
-    files: patches
-      .filter((patch) => !patch.allowedHostsCss || patch.allowedHostsCss.includes(tabHostname))
-      .reduce((acc, patch) => {
-        if (config[patch.name].enable && patch.files?.css?.length)
-          return [...acc, ...patch.files.css.map((file) => `patches/${file}`)];
-        return acc
-      }, []),
+  patches.forEach(patch => {
+    if (config[patch.name] !== undefined) return;
+    config[patch.name] = { description: patch.description, enable: true };
   });
 
-  const [{ result: isInitiated }] = await chrome.scripting.executeScript({
-    target: { tabId },
-    func: () => {
-      if (!window.modules) {
-        window.modules = []
-        return false
-      }
-      return true
-    }
-  })
+  chrome.storage.sync.set({ options: config });
 
-  if (!isInitiated) await chrome.scripting.executeScript({
-    target: { tabId },
-    files: patches
-      .reduce((acc, patch) => {
-        if (config[patch.name].enable && patch.files?.js?.length)
-          return [...acc, ...patch.files.js.map((file) => `patches/${file}`)];
-        return acc;
-      }, []),
-  })
+  chrome.storage.onChanged.addListener(function (changes, namespace) {
+    if (namespace !== "sync") return;
+    config = changes.options.newValue;
+    chrome.permissions.getAll((permissions) => {
+      const hostPatterns = permissions.origins || [];
 
-  chrome.scripting.executeScript({
-    target: { tabId },
-    func: () => {
-      if (!window.modules.length) return console.warn("Script tried executing before all files loaded or all patches are disabled")
-      const isFirstRun = !window.iWasThere; // :)
-      window.iWasThere = true
-
-      for (const { onlyOnReloads, doesRunHere, isLoaded, run } of window.modules) {
-        if (onlyOnReloads && !isFirstRun) continue
-        if (doesRunHere !== undefined && !doesRunHere()) continue
-
-        if (isLoaded === undefined) run()
-        else if (isLoaded()) run()
-        else {
-          const observer = new MutationObserver((mutationsList, observer) => {
-            if (!isLoaded()) return
-            observer.disconnect();
-            run();
+      hostPatterns.forEach((pattern) => {
+        chrome.tabs.query({ url: pattern }, (tabs) => {
+          tabs.forEach((tab) => {
+            chrome.tabs.reload(tab.id);
           });
-          observer.observe(document.body, {
-            subtree: true,
-            childList: true
-          })
-        }
-      }
-    }
+        });
+      });
+    });
   });
-});
+
+  chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    if (changeInfo.status !== "loading" || !/^http/.test(tab.url)) return;
+    const tabHostname = new URLPattern(tab.url).hostname;
+    if (!allowedHostnames.includes(tabHostname)) return;
+    chrome.scripting.insertCSS({
+      target: { tabId: tabId },
+      files: patches
+        .filter((patch) => !patch.allowedHostsCss || patch.allowedHostsCss.includes(tabHostname))
+        .reduce((acc, patch) => {
+          if (config[patch.name].enable && patch.files?.css?.length)
+            return [...acc, ...patch.files.css.map((file) => `patches/${file}`)];
+          return acc;
+        }, []),
+    });
+
+    const [{ result: isInitiated }] = await chrome.scripting.executeScript({
+      target: { tabId },
+      func: () => {
+        if (!window.modules) {
+          window.modules = [];
+          return false;
+        }
+        return true;
+      },
+    });
+
+    if (!isInitiated)
+      await chrome.scripting.executeScript({
+        target: { tabId },
+        files: patches.reduce((acc, patch) => {
+          if (config[patch.name].enable && patch.files?.js?.length)
+            return [...acc, ...patch.files.js.map((file) => `patches/${file}`)];
+          return acc;
+        }, []),
+      });
+
+    chrome.scripting.executeScript({
+      target: { tabId },
+      func: () => {
+        if (!window.modules.length)
+          return console.warn(
+            "Script tried executing before all files loaded or all patches are disabled"
+          );
+        const isFirstRun = !window.iWasThere; // :)
+        window.iWasThere = true;
+
+        for (const {
+          onlyOnReloads,
+          doesRunHere,
+          isLoaded,
+          run,
+        } of window.modules) {
+          if (onlyOnReloads && !isFirstRun) continue;
+          if (doesRunHere !== undefined && !doesRunHere()) continue;
+
+          if (isLoaded === undefined) run();
+          else if (isLoaded()) run();
+          else {
+            const observer = new MutationObserver((mutationsList, observer) => {
+              if (!isLoaded()) return;
+              observer.disconnect();
+              run();
+            });
+            observer.observe(document.body, {
+              subtree: true,
+              childList: true,
+            });
+          }
+        }
+      },
+    });
+  });
+}
+
+run();
