1927be145804a717e83dd612351468ac3f715d0c
banocean <47253870+banocean@users.noreply.github.com>
2024-09-07 12:54:26 +0200

    reformat code

--- apply.js
+++ apply.js
@@ -8,60 +8,83 @@
  */
 
 const allowedHostnames = [
-  "dziennik-uczen.vulcan.net.pl",
-  "dziennik-wiadomosci.vulcan.net.pl",
-  "dziennik-logowanie.vulcan.net.pl",
-  "uczen.eduvulcan.pl",
-  "wiadomosci.eduvulcan.pl",
-  "eduvulcan.pl",
+    "dziennik-uczen.vulcan.net.pl",
+    "dziennik-wiadomosci.vulcan.net.pl",
+    "dziennik-logowanie.vulcan.net.pl",
+    "uczen.eduvulcan.pl",
+    "wiadomosci.eduvulcan.pl",
+    "eduvulcan.pl",
 ];
 
 /** @returns {Promise<Patch[]>} */
 const fetchPatches = async () => {
-  const patchesResponse = await fetch(chrome.runtime.getURL("patches.json"))
-  return JSON.parse(await patchesResponse.text());
-}
+    const patchesResponse = await fetch(chrome.runtime.getURL("patches.json"));
+    return await patchesResponse.json();
+};
 
-const getConfig = async () => (await chrome.storage.sync.get("options"))?.options ?? {};
+const getConfig = async () =>
+    (await chrome.storage.sync.get("options"))?.options ?? {};
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
-  if (!changes.options?.oldValue || changes.options?.oldValue === changes.options?.newValue) return
-  if (namespace !== "sync") return;
-  window.location.reload()
-})
+    if (
+        !changes.options?.oldValue ||
+        changes.options?.oldValue === changes.options?.newValue
+    )
+        return;
+    if (namespace !== "sync") return;
+    window.location.reload();
+});
 
 const getPatchesFiles = (patches, config) => {
-  return patches.flatMap((patch) => {
-    const result = []
-    if (config[patch.name].enable) {
-      if (patch.files?.js?.length) result.push(patch.files.js.map((file) => `patches/${file}`));
-      if ((!patch.allowedHostsCss || patch.allowedHostsCss.includes(window.location.hostname)) && patch.files?.css?.length) {
-        result.push(patch.files.css.map((file) => `patches/${file}`))
-      }
-    }
-    return result
-  }).flat()
-}
-
+    return patches
+        .flatMap((patch) => {
+            const result = [];
+            if (config[patch.name].enable) {
+                if (patch.files?.js?.length)
+                    result.push(
+                        patch.files.js.map((file) => `patches/${file}`),
+                    );
+                if (
+                    (!patch.allowedHostsCss ||
+                        patch.allowedHostsCss.includes(
+                            window.location.hostname,
+                        )) &&
+                    patch.files?.css?.length
+                ) {
+                    result.push(
+                        patch.files.css.map((file) => `patches/${file}`),
+                    );
+                }
+            }
+            return result;
+        })
+        .flat();
+};
 
 async function run() {
-  let config = await getConfig()
-  const patches = await fetchPatches()
+    let config = await getConfig();
+    const patches = await fetchPatches();
 
-  patches.forEach(patch => {
-    if (config[patch.name] !== undefined) return;
-    config[patch.name] = { description: patch.description, enable: true };
-  });
+    patches.forEach((patch) => {
+        if (config[patch.name] !== undefined) return;
+        config[patch.name] = { description: patch.description, enable: true };
+    });
 
-  chrome.storage.sync.set({ options: config });
+    chrome.storage.sync.set({ options: config });
 
-  for (const filePath of getPatchesFiles(patches, config)) {
-    const element = document.createElement(filePath.endsWith(".js") ? "script" : "link")
-    element.setAttribute(filePath.endsWith(".js") ? "src" : "href", chrome.runtime.getURL(filePath))
-    if (filePath.endsWith(".css")) element.setAttribute("rel", "stylesheet")
-    else element.classList.add("injected-script")
-    document.head.appendChild(element)
-  }
+    for (const filePath of getPatchesFiles(patches, config)) {
+        const element = document.createElement(
+            filePath.endsWith(".js") ? "script" : "link",
+        );
+        element.setAttribute(
+            filePath.endsWith(".js") ? "src" : "href",
+            chrome.runtime.getURL(filePath),
+        );
+        if (filePath.endsWith(".css"))
+            element.setAttribute("rel", "stylesheet");
+        else element.classList.add("injected-script");
+        document.head.appendChild(element);
+    }
 }
 
-run()
+run();
--- execute.js
+++ execute.js
@@ -1,32 +1,27 @@
-const modules = []
+const modules = [];
 window.appendModule = (...args) => {
-    modules.push(...args)
-    if (document.querySelectorAll(".injected-script").length === modules.length) {
-        execute()
-
+    modules.push(...args);
+    if (
+        document.querySelectorAll(".injected-script").length === modules.length
+    ) {
+        execute();
     }
-}
+};
 
 const execute = () => {
     if (!modules.length)
         console.warn(
-            "Script tried executing before all files loaded or all patches are disabled"
+            "Script tried executing before all files loaded or all patches are disabled",
         );
 
     const isFirstRun = !window.iWasThere; // :)
     window.iWasThere = true;
 
-    for (const {
-        onlyOnReloads,
-        doesRunHere,
-        isLoaded,
-        run,
-    } of modules) {
+    for (const { onlyOnReloads, doesRunHere, isLoaded, run } of modules) {
         if (onlyOnReloads && !isFirstRun) continue;
         if (doesRunHere !== undefined && !doesRunHere()) continue;
 
-        if (isLoaded === undefined) run();
-        else if (isLoaded()) run();
+        if (!isLoaded || isLoaded()) run();
         else {
             const observer = new MutationObserver((mutationsList, observer) => {
                 if (!isLoaded()) return;
@@ -39,11 +34,11 @@ const execute = () => {
             });
         }
     }
-}
+};
 
 window.history.pushState = new Proxy(window.history.pushState, {
     apply: (target, a, args) => {
         target.apply(a, args);
-        execute()
+        execute();
     },
 });
