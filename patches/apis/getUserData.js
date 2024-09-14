dcd5b22aef996033303e4380e9cebd9c80200264
banocean <47253870+banocean@users.noreply.github.com>
2024-09-14 16:54:25 +0200

    wait for data to render

--- patches/apis/getUserData.js
+++ patches/apis/getUserData.js
@@ -1,16 +1,27 @@
 window.getUserData = async () => {
-    return await window.getFromAside(() => ({
-        fullName: window.location.hostname.includes("wiadomosci")
-            ? document
-                  .querySelector(".account__name span")
-                  ?.firstChild?.textContent?.split(" ")
-                  .reverse()
-                  .join(" ")
-            : document.querySelector(".side_important-text.side_student")
-                  ?.textContent,
-        username: document.querySelector(".user div:nth-child(2)").lastChild
-            .textContent,
-    }));
+    return await window.getFromAside(async () => {
+        await window.waitForRender(
+            () =>
+                document.querySelector(
+                    window.location.hostname.includes("wiadomosci")
+                        ? ".account__name span > *"
+                        : ".side_important-text.side_student",
+                ) && document.querySelector(".user__links__conatainer"),
+        );
+
+        return {
+            fullName: window.location.hostname.includes("wiadomosci")
+                ? document
+                      .querySelector(".account__name span")
+                      ?.firstChild?.textContent?.split(" ")
+                      .reverse()
+                      .join(" ")
+                : document.querySelector(".side_important-text.side_student")
+                      ?.textContent,
+            username: document.querySelector(".user div:nth-child(2)").lastChild
+                .textContent,
+        };
+    });
 };
 
 window.skipModule();
