716651af105895ccd5a7b18bc324b97d110ff7ea
banocean <47253870+banocean@users.noreply.github.com>
2024-09-20 21:46:24 +0200

    move inside of the details element

--- patches/hideSubjectsWithNoGrades/script.js
+++ patches/hideSubjectsWithNoGrades/script.js
@@ -1,45 +1,59 @@
+const SEMESTERS = 2;
 const renderVisibilityButtons = () => {
-    const SEMESTERS = 2;
     for (let i = 0; i < SEMESTERS; i++) {
         const isVisible = localStorage.getItem(`hiddenSubjectsVisible:${i}`) === "true"
         toggleVisible(i, isVisible)
     }
 }
 
-const toggleVisible = (i, isVisible) => {
-    const container = document.querySelector("section > section > .mobile__frame > .content-container").children
-    debugger
+// For some reason vulcan (prob react does that) overwrites entire classList when component is folded :/
+const watchForClassListChanges = async (i) => {
+    await window.waitForRender(() => getSemestersContainer()[i])
+    const observer = new MutationObserver(() => {
+        const isVisible = localStorage.getItem(`hiddenSubjectsVisible:${i}`) === "true"
+        toggleVisible(i, isVisible)
+    })
+    getSemestersContainer()[i].addEventListener("click")
+}
+
+const getSemestersContainer = () => document.querySelector("section > section > .mobile__frame > .content-container").children
+
+const toggleVisible = async (i, isVisible) => {
+    await window.waitForRender(() => getSemestersContainer()[i].querySelector("article"), document.querySelector("section > section > .mobile__frame > .content-container"))
+    const container = getSemestersContainer()[i]
     if (isVisible !== undefined) {
-        if (isVisible) container[i].classList.remove("hide-subjects")
-        else container[i].classList.add("hide-subjects")
-        debugger
+        if (isVisible) {
+            container.classList.remove("hide-subjects")
+        }
+        else {
+            container.classList.add("hide-subjects")
+        }
     } else {
-        container[i].classList.toggle("hide-subjects")
+        container.classList.toggle("hide-subjects")
     }
-    isVisible = container[i].classList.contains("hide-subjects")
+    isVisible = !container.classList.contains("hide-subjects")
     localStorage.setItem(`hiddenSubjectsVisible:${i}`, isVisible)
-    renderSettings(container[i], isVisible, i)
+    renderSettings(container, isVisible, i)
 }
 
 const createInfoElement = (container) => {
     const element = document.createElement("div")
     element.classList.add("hidden-subjects-info")
-    container.insertBefore(element, container.children[1])
+    container.children[1].insertBefore(element, container.children[1].firstChild)
     return element
 }
 
 const renderSettings = (container, isVisible, i) => {
-    const secondChild = container.firstChild.children[1]
-    debugger
+    const secondChild = container.children[1]
     const element =
-        secondChild.classList.contains("hidden-subjects-info") ? secondChild : createInfoElement(container)
+        secondChild.firstChild.classList.contains("hidden-subjects-info") ? secondChild.firstChild : createInfoElement(container)
     const numberOfSubjects = Array.from(container.querySelectorAll("article:has(.tile__content:first-of-type:empty)")).length
-    debugger
     if (numberOfSubjects === 0) {
         element.style.display = "none"
     } else {
-        debugger
-        element.innerHTML = `Ukryto ${numberOfSubjects} przedmiotów bez ocen <button>${isVisible ? "Ukryj" : "Pokaż"}</button>`
+        const FOLD_ICON_URL = "https://raw.githubusercontent.com/banocean/ifv/hswng/assets/icons/unfold_less_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg"
+        const UNFOLD_ICON_URL = "https://raw.githubusercontent.com/banocean/ifv/hswng/assets/icons/unfold_more_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg"
+        element.innerHTML = `${isVisible ? "Pokazano" : "Ukryto"} ${numberOfSubjects} przedmiotów bez ocen <button><img src="${isVisible ? `${FOLD_ICON_URL}">Ukryj` : `${UNFOLD_ICON_URL}">Pokaż`}</button>`
         element.querySelector("button").addEventListener("click", () => toggleVisible(i))
     }
 }
