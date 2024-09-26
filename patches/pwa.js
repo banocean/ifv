0e6a0287d9df28ead53077371932aa7510a8b527
JeżyQ <80815350+PanLiszka@users.noreply.github.com>
2024-09-26 17:27:03 +0200

    Web manifest url change (#75)

--- patches/pwa.js
+++ patches/pwa.js
@@ -2,19 +2,34 @@ function injectWebManifest() {
     const metaLink = document.createElement("link");
 
     metaLink.setAttribute("rel", "manifest");
-    metaLink.setAttribute(
-        "href",
-        window.location.hostname === "eduvulcan.pl" ?
-            "https://raw.githubusercontent.com/banocean/ifv/main/pwa/manifest-eduvulcan.json" :
-            `https://ifv-pwa.banocean.com/${window.location.pathname.split("/")[1]}`,
-    );
+    metaLink.setAttribute("href", getManifestLink(window.location.hostname));
 
     document.head.appendChild(metaLink);
+}
+
+function getManifestLink(hostname) {
+    switch (hostname) {
+        case "eduvulcan.pl":
+            return "https://raw.githubusercontent.com/banocean/ifv/main/pwa/manifest-eduvulcan.json";
+        case "uczen.eduvulcan.pl":
+            return `https://ifv-pwa.banocean.com/eduvulcan/${
+                window.location.pathname.split("/")[1]
+            }`;
+        case "dziennik-uczen.vulcan.net.pl":
+            return `https://ifv-pwa.banocean.com/dziennik/${
+                window.location.pathname.split("/")[1]
+            }`;
+    }
 }
 
 window.appendModule({
     isLoaded: () => true,
     onlyOnReloads: true,
     run: injectWebManifest,
-    doesRunHere: () => ["eduvulcan.pl", "dziennik-uczen.vulcan.net.pl"].includes(window.location.hostname),
+    doesRunHere: () =>
+        [
+            "eduvulcan.pl",
+            "uczen.eduvulcan.pl",
+            "dziennik-uczen.vulcan.net.pl",
+        ].includes(window.location.hostname),
 });
