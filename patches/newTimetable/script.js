0d8c6d0cb925e3695727433c70eac4fdda304680
banocean <47253870+banocean@users.noreply.github.com>
2024-09-27 20:16:53 +0200

    collect data about the day

--- patches/newTimetable/script.js
+++ patches/newTimetable/script.js
@@ -1,56 +1,61 @@
 const mapData = () =>
     Array.from(document.querySelectorAll(".app__content section")).map(
-        (element) =>
-            Array.from(
-                element.querySelectorAll(
-                    ".cell--single, .cell--multi--conflicted",
-                ),
-            ).map((lesson) => {
-                const hoursText = (
-                    lesson.querySelector(".position__lesson__hours")
-                        ?.innerText || "  "
-                ).split(" ");
-                const startingHour = hoursText[0];
-                const endingHour = hoursText[2];
+        (element) => {
+            return {
+                note: element.querySelector(".plan-zajec__accordion__wolne")?.innerText,
+                day: element.querySelector(".MuiAccordionSummary-content > h2")?.innerText,
+                lessons: Array.from(
+                    element.querySelectorAll(
+                        ".cell--single, .cell--multi--conflicted",
+                    ),
+                ).map((lesson) => {
+                    const hoursText = (
+                        lesson.querySelector(".position__lesson__hours")
+                            ?.innerText || "  "
+                    ).split(" ");
+                    const startingHour = hoursText[0];
+                    const endingHour = hoursText[2];
 
-                const subjectText =
-                    lesson
-                        .querySelector(".position__lesson__subject")
-                        ?.innerText?.split(" Grupa-") || [];
+                    const subjectText =
+                        lesson
+                            .querySelector(".position__lesson__subject")
+                            ?.innerText?.split(" Grupa-") || [];
 
-                const annotationText = lesson.querySelector(
-                    ".plan-position__adnotation-title",
-                )?.innerText;
+                    const annotationText = lesson.querySelector(
+                        ".plan-position__adnotation-title",
+                    )?.innerText;
 
-                const type = lesson.classList.contains(
-                    "cell--multi--conflicted",
-                )
-                    ? "conflicted"
-                    : lesson.querySelector(".zastepstwo")
-                      ? "substitute"
-                      : lesson.querySelector(".odwolane")
-                        ? "canceled"
-                        : annotationText
-                          ? "unknown"
-                          : "normal";
+                    const type = lesson.classList.contains(
+                        "cell--multi--conflicted",
+                    )
+                        ? "conflicted"
+                        : lesson.querySelector(".zastepstwo")
+                            ? "substitute"
+                            : lesson.querySelector(".odwolane")
+                                ? "canceled"
+                                : annotationText
+                                    ? "unknown"
+                                    : "normal";
 
-                return {
-                    originalElement: lesson,
-                    type,
-                    subject: subjectText[0],
-                    group: subjectText[1],
-                    classroom: [
-                        ...(lesson.querySelector(
-                            ".position__lesson__subject + span",
-                        )?.innerText || ""),
-                    ]
-                        .filter((c) => !"()".includes(c))
-                        .join(""),
-                    annotationText,
-                    startingHour,
-                    endingHour,
-                };
-            }),
+                    return {
+                        originalElement: lesson,
+                        type,
+                        subject: subjectText[0],
+                        group: subjectText[1],
+                        classroom: [
+                            ...(lesson.querySelector(
+                                ".position__lesson__subject + span",
+                            )?.innerText || ""),
+                        ]
+                            .filter((c) => !"()".includes(c))
+                            .join(""),
+                        annotationText,
+                        startingHour,
+                        endingHour,
+                    };
+                }),
+            }
+        }
     );
 
 const isOpened = (element) => element.querySelector(".MuiCollapse-root")?.style?.minHeight === "0px"
