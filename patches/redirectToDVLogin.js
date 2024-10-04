d431cabb430f97299242287459760e9ca118657c
rotkiW285 <105926196+rotkiW285@users.noreply.github.com>
2024-10-04 19:34:41 +0200

    Refactor span in DV patch

--- patches/redirectToDVLogin.js
+++ patches/redirectToDVLogin.js
@@ -9,3 +9,22 @@ window.appendModule({
         window.location.hostname === "dziennik-uczen.vulcan.net.pl"
 		&& !window.location.pathname.split("/")[2]
 });
+
+const pathSegment = window.location.pathname.split("/")[1];
+
+const newSpanContent = `
+  Nie pamiętasz loginu lub hasła? Możemy wysłać na Twój <nobr>e-mail</nobr> link do zresetowania dostępu. 
+  Przejdź do <a href="https://dziennik-logowanie.vulcan.net.pl/${pathSegment}/PasswordReset/UnlockRequest" class="link-simple">formularza</a> 
+  lub użyj <a href="https://uonetplus.vulcan.net.pl/${pathSegment}" class="link-simple">innej metody logowania</a>
+`;
+
+const spanElements = document.querySelectorAll('span');
+
+spanElements.forEach(span => {
+  if (span.querySelector('nobr')) {
+    span.innerHTML = newSpanContent;
+  }
+});
+
+
+
