475317ea01d93fdbf375e527cf3b677ba4bff0ab
Wiktor Stawicki <105926196+rotkiW285@users.noreply.github.com>
2024-10-08 19:11:36 +0200

    Redirect to login page on `dziennik-uczen.vulcan.net.pl` (#56)
    Co-authored-by: KPniX <78750235+kpnix@users.noreply.github.com>

---
+++ patches/redirectToDVLogin.js
@@ -0,0 +1,26 @@
+function redirectToLoginPage() {
+  window.location.pathname = `/${
+    window.location.pathname.split("/")[1]
+  }/LoginEndpoint.aspx`;
+}
+
+window.appendModule({
+  onlyOnReloads: true,
+  run: redirectToLoginPage,
+  doesRunHere: () =>
+    window.location.hostname === "dziennik-uczen.vulcan.net.pl" &&
+    !window.location.pathname.split("/")[2],
+});
+
+const pathSegment = window.location.pathname.split("/")[1];
+
+const targetSpan = document.querySelector(
+  ".input-components ~ .form-gap > span"
+);
+
+if (targetSpan) {
+  const spanText = targetSpan.textContent;
+  const newText = spanText.slice(0, -1);
+  const additionalText = ` lub użyj <a href="https://uonetplus.vulcan.net.pl/${pathSegment}" class="link-simple">innej metody logowania</a>`;
+  targetSpan.innerHTML = `${newText}${additionalText}`;
+}
