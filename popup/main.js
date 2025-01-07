511e15125a62bcf04d97565276e1e0fb465a2ec2
ocean <47253870+banocean@users.noreply.github.com>
2025-01-07 18:23:47 +0100

    fix toggle all

--- popup/main.js
+++ popup/main.js
@@ -3,40 +3,31 @@ const fetchPatches = async () => {
     return await patchesResponse.json();
 };
 
-if (/\bMobile\b/.test(navigator.userAgent)) document.body.classList.add("mobile");
+if (/\bMobile\b/.test(navigator.userAgent))
+    document.body.classList.add("mobile");
 
 const filterInput = document.querySelector(".filter > input");
 
 filterInput.addEventListener("input", () => {
     const filter = filterInput.value.toLowerCase();
-    Array.from(document.querySelector(".options").children).forEach((option) => {
-        option.style.display =
-            option.querySelector(".title").innerText.toLowerCase().includes(filter)
-                || option.querySelector(".desc").innerText.toLowerCase().includes(filter) 
-                ? "flex" : "none";
-    });
+    Array.from(document.querySelector(".options").children).forEach(
+        (option) => {
+            option.style.display =
+                option
+                    .querySelector(".title")
+                    .innerText.toLowerCase()
+                    .includes(filter) ||
+                option
+                    .querySelector(".desc")
+                    .innerText.toLowerCase()
+                    .includes(filter)
+                    ? "flex"
+                    : "none";
+        },
+    );
 });
 
-(async () => {
-    const changeAllButton = document.querySelector(".filter > button");
-    let nextApplyAllAction = ((await chrome.storage.sync.get("nextApplyAllAction")) || false);
-    
-    const toggleChangeAllButton = () => {
-        chrome.storage.sync.set({ "nextApplyAllAction": !nextApplyAllAction });
-        document.querySelectorAll(".options input").forEach((option) => {
-            option.checked = nextApplyAllAction;
-        });
-        setButtonName();
-        nextApplyAllAction = !nextApplyAllAction;
-    };
-
-    const setButtonName = () => document.querySelector(".filter > button").innerHTML = nextApplyAllAction ? "Disable&nbsp;All" : "Enable&nbsp;All";
-
-    setButtonName();
-    changeAllButton.addEventListener("click", toggleChangeAllButton);
-})();
-
-document.addEventListener("DOMContentLoaded", async () => {
+const render = async () => {
     let config = (await chrome.storage.sync.get("options"))?.options ?? {};
     const patches = await fetchPatches();
 
@@ -47,18 +38,19 @@ document.addEventListener("DOMContentLoaded", async () => {
     chrome.storage.sync.set({ options: config });
 
     const optionsDOM = document.querySelector(".options");
+    optionsDOM.innerHTML = "";
     for (const [key, value] of Object.entries(config)) {
         const option = document.createElement("label");
         option.innerHTML = `
-        <div style="flex: 1;">
-            <p class="title">${key}</p>
-            <p class="desc">${value.description}</p>
-        </div>
-        <div class="toggle-wrapper">
-            <input class="toggle-input" type="checkbox" ${value.enable ? "checked" : ""}>
-            <div class="toggle-switch"></div>
-        </div>
-    `;
+            <div style="flex: 1;">
+                <p class="title">${key}</p>
+                <p class="desc">${value.description}</p>
+            </div>
+            <div class="toggle-wrapper">
+                <input class="toggle-input" type="checkbox" ${value.enable ? "checked" : ""}>
+                <div class="toggle-switch"></div>
+            </div>
+        `;
         option.querySelector("input").id = key;
         optionsDOM.appendChild(option);
     }
@@ -70,4 +62,40 @@ document.addEventListener("DOMContentLoaded", async () => {
             await chrome.storage.sync.set({ options: config });
         }
     });
-});
+};
+
+(async () => {
+    const changeAllButton = document.querySelector(".filter > button");
+    let nextApplyAllAction =
+        (await chrome.storage.sync.get("nextApplyAllAction"))
+            .nextApplyAllAction || false;
+
+    const toggleAllButton = async () => {
+        chrome.storage.sync.set({ nextApplyAllAction: !nextApplyAllAction });
+
+        let config = {};
+        const patches = await fetchPatches();
+
+        patches.forEach((patch) => {
+            if (config[patch.name] !== undefined) return;
+            config[patch.name] = {
+                description: patch.description,
+                enable: nextApplyAllAction,
+            };
+        });
+
+        await chrome.storage.sync.set({ options: config });
+        await render();
+        nextApplyAllAction = !nextApplyAllAction;
+        setButtonName();
+    };
+
+    const setButtonName = () =>
+        (document.querySelector(".filter > button").innerHTML =
+            nextApplyAllAction ? "Enable&nbsp;All" : "Disable&nbsp;All");
+
+    setButtonName();
+    changeAllButton.addEventListener("click", toggleAllButton);
+})();
+
+document.addEventListener("DOMContentLoaded", render);
