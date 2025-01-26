3e93e3eff0ea50ee587a4ecfa7ac9545400e7fb5
ocean <47253870+banocean@users.noreply.github.com>
2025-01-26 20:24:37 +0100

    fix on desktop

--- patches/newAttendance/justifyButton.js
+++ patches/newAttendance/justifyButton.js
@@ -6,14 +6,14 @@ const createButton = () => {
     button.addEventListener("click", () => {
         document.querySelector(".app__content__header > .toolbar > button").click()
     })
-    document.querySelector(".app__content > .mobile__frame").appendChild(button)
+    document.querySelector(window.innerWidth < 1024 ? ".app__content > .mobile__frame" : ".app__content > .desktop__frame").appendChild(button)
 }
 
 window.appendModule({
     run: createButton,
     doesRunHere: () => window.location.href.endsWith("frekwencja"),
     onlyOnReloads: false,
-    isLoaded: () => document.querySelector(".app__content > .mobile__frame")
+    isLoaded: () => document.querySelector(".app__content > .mobile__frame, .app__content > .desktop__frame")
         && document.querySelector(".app__content__header > .toolbar > button")
         && !document.querySelector(".spinner")
 });
