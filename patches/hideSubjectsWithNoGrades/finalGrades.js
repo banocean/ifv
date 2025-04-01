91d753f6cdf6d483abe04b9f5a026a41bafcec3d
YOPER12 <69840031+YOPER12@users.noreply.github.com>
2025-04-01 20:44:56 +0200

    remove timeouts

--- patches/hideSubjectsWithNoGrades/finalGrades.js
+++ patches/hideSubjectsWithNoGrades/finalGrades.js
@@ -1,48 +1,43 @@
-import { waitForRender } from "../apis/waitForElement.js";
+import { waitForRender, waitForReplacement } from "../apis/waitForElement.js";
 
 const hideEmptyColumns = async () => {
+    await waitForRender(() => document.querySelector(".p-datatable-table .details-btn--appearance"));
+
+    const headers = document.querySelectorAll('.p-datatable-table th');
+
+    headers.forEach((header, idx) => {
+        const cells = Array.from(document.querySelectorAll('tbody tr td:nth-child(' + (idx + 1) + ')'));
+        const check = cells.some(cell => cell.textContent.trim().length > 0);
+
+        const columnCells = document.querySelectorAll('tr th:nth-child(' + (idx + 1) + '), tr td:nth-child(' + (idx + 1) + ')');
+        columnCells.forEach(cell => {
+            cell.style.display = check ? '' : 'none';
+        });
+    });
+};
+
+async function prep() {
+
     if (window.innerWidth > 1024) {
-        await waitForRender(() => document.querySelector(".p-datatable-table .details-btn--appearance"));
+        await waitForRender(() => document.querySelector(".MuiTabs-flexContainer > button"));
 
-        const headers = document.querySelectorAll('.p-datatable-table th');
-
-        headers.forEach((header, idx) => {
-            const cells = Array.from(document.querySelectorAll('tbody tr td:nth-child(' + (idx + 1) + ')'));
-            const check = cells.some(cell => cell.textContent.trim().length > 0);
-
-            const columnCells = document.querySelectorAll('tr th:nth-child(' + (idx + 1) + '), tr td:nth-child(' + (idx + 1) + ')');
-            columnCells.forEach(cell => {
-                cell.style.display = check ? '' : 'none';
+        hideEmptyColumns();
+        document.querySelectorAll(".MuiTabs-flexContainer > button").forEach(e => {
+            e.addEventListener("click", async () => {
+                await waitForReplacement(() => document.querySelector('.p-datatable-table .details-btn--appearance'));
+                hideEmptyColumns();
             });
         });
     } else {
-        await waitForRender(() => document.querySelector(".MuiAccordionDetails-root .grades__box .info-row"));
-        setTimeout(() => {
-            document.querySelectorAll('.info-row').forEach(e => {
+        await waitForRender(() => document.querySelector(".MuiAccordionDetails-root.accordion__full-width__content > .mobile__frame .grades__box"))
+        document.querySelectorAll('.MuiAccordionDetails-root.accordion__full-width__content > .mobile__frame').forEach(async semester => {
+            console.debug('semester', semester);
+            await waitForRender(() => semester.querySelector('.MuiAccordionDetails-root .grades__box .info-row .info-text > span'));
+            semester.querySelectorAll('.info-row').forEach(e => {
                 if (e.querySelector('.info-text > span').textContent.trim() === "" || e.querySelector('.info-text > span').textContent.trim() === "0") {
                     e.style.display = "none";
                 }
             });
-        }, 100)
-    }
-};
-
-async function prep() {
-    hideEmptyColumns()
-
-    if (window.innerWidth > 1024) {
-        await waitForRender(() => document.querySelector('.MuiTabs-flexContainer > button'))
-        document.querySelectorAll(".MuiTabs-flexContainer > button").forEach(e => {
-            e.addEventListener("click", () => {
-                setTimeout(hideEmptyColumns, 100)
-            })
-        })
-    } else {
-        await waitForRender(() => document.querySelector('.MuiPaper-root .accordion__full-width__header'))
-        document.querySelectorAll('.MuiPaper-root .accordion__full-width__header').forEach(e => {
-            e.addEventListener("click", () => {
-                setTimeout(hideEmptyColumns, 100);
-            })
         })
     }
 }
@@ -52,5 +47,4 @@ window.appendModule({
     onlyOnReloads: false,
     doesRunHere: () =>
         window.location.pathname.endsWith("oceny"),
-    isLoaded: () => true,
 });
