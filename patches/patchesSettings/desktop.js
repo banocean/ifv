1a655956af47a0f192e42cd8ed102937ccfd3a9a
YOPER12 <69840031+YOPER12@users.noreply.github.com>
2025-06-17 10:04:07 +0200

    prevent scrolling when modal active

--- patches/patchesSettings/desktop.js
+++ patches/patchesSettings/desktop.js
@@ -44,6 +44,7 @@ async function hideModal() {
     modalDiv.style.transform = "translate(-50%, 200%)";
     modalDiv.style.opacity = "0.3";
     modalBackground.style.background = "rgba(0, 0, 0, 0)";
+    document.body.style.overflow = "auto";
     setTimeout(() => {
         modalDiv.style.zIndex = "-1";
         modalBackground.style.zIndex = "-1";
@@ -57,6 +58,7 @@ async function showModal() {
     modalDiv.style.opacity = "1";
     modalBackground.style.background = "rgba(0, 0, 0, 0.5)";
     modalBackground.style.zIndex = "999";
+    document.body.style.overflow = "hidden";
 }
 
 window.appendModule({
